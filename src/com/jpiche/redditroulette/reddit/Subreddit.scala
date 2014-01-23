fe51b4db6a68d9e328367b2112cca676df6b89ab
Joseph J. Piché <j@jpiche.com>
2014-01-23 09:19:10 -0600

    First stab at adding cache for subreddit listings

--- src/com/jpiche/redditroulette/reddit/Subreddit.scala
+++ src/com/jpiche/redditroulette/reddit/Subreddit.scala
@@ -2,13 +2,14 @@ package com.jpiche.redditroulette.reddit
 
 import argonaut._, Argonaut._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Future, promise}
+import scala.concurrent.{Future, promise, future}
 import scala.util.{Failure, Success, Random}
 import com.jpiche.redditroulette._
 import android.util.Log
 import scala.Some
 import com.jpiche.redditroulette.net._
-
+import java.util.concurrent.atomic.AtomicInteger
+import scala.collection.mutable
 
 case class SubredditData(
   kind: String,
@@ -27,14 +28,37 @@ case class Subreddit(
   lazy val hot = url + "hot.json?limit=100"
 
   def next(implicit webSettings: WebSettings, prefs: Prefs): Future[(WebData, Thing)] =
-    Web.get(hot) collect {
-      case web: WebData => Subreddit.randomThing(web)
+    Subreddit.listCache.get(name) match {
+      // TODO: make the number a variable somewhere?
+      case Some((web, listing)) if Subreddit.listCacheInc.getOrElse(name, 0) < 20 =>
+        future {
+          // increment the number of times we've used this
+          // TODO: is this thread safe?
+          Subreddit.listCacheInc put (name, Subreddit.listCacheInc.getOrElse(name, 1))
+          Subreddit.listCache put (name, (web, listing))
+          (web, Subreddit.randomThing(listing))
+        }
+      case _ =>
+        Web.get(hot) collect {
+          case web: WebData =>
+            web.asString.decodeOption[Listing] match {
+              case Some(list) if list.hasChildren =>
+                // every time this is re-fetched, reset the counter to zero
+                Subreddit.listCacheInc put (name, 0)
+                Subreddit.listCache put (name, (web, list))
+                (web, Subreddit.randomThing(list))
+              case None => throw new Exception
+            }
 
-      case fail: WebFail => throw new Exception(fail.errorMessage)
+          case fail: WebFail => throw new Exception(fail.errorMessage)
+        }
     }
 }
 
 object Subreddit extends LogTag {
+
+  private val listCache = mutable.HashMap.empty[String, (WebData, Listing)]
+  private val listCacheInc = mutable.HashMap.empty[String, Int]
 
   private def aboutUrl(name: CharSequence) = s"http://www.reddit.com/r/$name/about.json"
 
@@ -89,22 +113,18 @@ object Subreddit extends LogTag {
     p.future
   }
 
-  def randomThing(web: WebData)(implicit prefs: Prefs): (WebData, Thing) =
-    web.asString.decodeOption[Listing] match {
-      case Some(listing: Listing) if listing.hasChildren =>
-        val children = if (prefs.showSelf)
-          listing.children
-        else
-          listing.children filterNot { _.data.isSelf }
+  def randomThing(listing: Listing)(implicit prefs: Prefs): Thing = {
+    val children = if (prefs.showSelf)
+      listing.children
+    else
+      listing.children filterNot { _.data.isSelf }
 
-        val t = {
-          val i = Random.nextInt(children.length)
-          children(i).data
-        }
-
-        Log.d(LOG_TAG, "collecting thing with url: %s" format t.url)
-        (web, t)
-
-      case _ => throw new Exception
+    val t = {
+      val i = Random.nextInt(children.length)
+      children(i).data
     }
+
+    Log.d(LOG_TAG, "collecting thing with url: %s" format t.url)
+    t
+  }
 }
